8a202484778f3ce4e5acad06ecdff4004e35113a
ant_reyn <antreyn@udel.edu>
2022-04-04 19:07:00 -0400

    added a feature that removes the hide class to show the value when clicked

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const inputBtn = document.querySelector(".btn");
 const showValue = document.querySelector(".user-input");
 const inputValue = document.querySelector("#typed-input");
 
-console.log(inputBtn);
-console.log(showValue);
-
 inputBtn.addEventListener("click", showInput);
 
 function showInput() {
   showValue.textContent = inputValue.value;
+  showValue.classList.remove("hide");
 }
